bf048c17093df3ef79d532233a2fba1704f6ca2a
Babak Khodabandeh <babak.khodabandeh@gmail.com>
2020-03-28 16:36:39 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,31 @@
-# Define the variable board below.
+row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# Position 1: left
+row[0]
+
+# Position 2: Middle
+row[1]
+ 
+# Position 3: Right
+row[2]# Define the variable board below.
+
+# Position 1: left
+row[3]
+
+# Position 2: Middle
+row[4]
+ 
+# Position 3: Right
+row[5]
+
+# Position 1: left
+row[6]
+
+# Position 2: Middle
+row[7]
+ 
+# Position 3: Right
+row[8]
+
+# Position 3: Right
+row[9]
